fdddada743a2621b4858d8bace5c237f650c4574
subratkumarsls <subratkumarsls.123@gmail.com>
2025-06-10 13:05:33 +0530

    Day  1 tak

---
+++ Day1/day1.py
@@ -0,0 +1,64 @@
+Name = "subrat"
+Age = 12
+Hobby = "coding"
+print(f"My name is {Name}. My age is {Age}. My hobby is {Hobby}.")
+# dynamic type conversion
+x = 10
+print(type(x)) 
+x = "Ten"
+print(type(x))  
+# data types int float and double
+Car_License_Plate = "MH1234"
+Car_Weight= 1234.56
+# Naming conventions 
+# Camel Case
+phoneModel = "iPhone 14 Pro"
+# Pascal Case
+PhoneBrand = "Apple"
+# Snake Case
+release_year = 2022
+
+print("Phone Model:", phoneModel)
+print("Phone Brand:", PhoneBrand)
+print("Release Year:", release_year)
+# boolena flip
+hungry=False
+print(f"Am i hungry? {hungry}")
+hungry=True
+print(f"Am i hungry?{hungry}")
+#school registration
+student_name = "Subrat"
+student_age = 21
+student_height = 5.7
+is_scholarship = True
+print(f"My name is {student_name}, I'm {student_age} years old, my height is {student_height}, and it is {is_scholarship} that I have a scholarship.")
+#multi line output
+name = "Subrat Kumar"
+address = "Bhubaneswar"
+pincode = "751003"
+phone = "8093571450"
+print(f"Name: {name}")
+print(f"Address: {address}")
+print(f"Pincode: {pincode}")
+print(f"Mobile: {phone}")
+print(f"On one line: {name}, {address}, {pincode}, {phone}")
+#store inventory 
+item_name = "Code Notebook"
+item_price = 2.5
+stock_quantity = 30
+print(type(item_name))    
+print(type(item_price))     
+print(type(stock_quantity))  
+print(f"We have {stock_quantity} units of {item_name}, each at Rs{item_price}.")
+# Favouratie Items
+fav_color = "Blue"
+fav_food = "Biryani"
+fav_movie = "Alice in Boderland"
+print(f"My favorites are: Color-{fav_color}, Food-{fav_food}, Movie-{fav_movie}")
+print(type(fav_color))
+print(type(fav_food))
+print(type(fav_movie))
+
+
+
+
